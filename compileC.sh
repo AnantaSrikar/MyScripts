9ec55cdea73f2bdba637e543093bda5ffb9fb8f4
AnantaSrikar <srikarananta01@gmail.com>
2020-02-24 01:06:36 +0530

    Added C compiler and executer

---
+++ compileC.sh
@@ -0,0 +1,18 @@
+function compile(){
+    fullFileName=$1
+    fileName=$(basename "$fullFileName")
+    fName="${fileName%.*}"
+    gcc $fullFileName -o $fName
+    ./$fName
+}
+
+function compileM(){
+    fullFileName=$1
+    fileName=$(basename "$fullFileName")
+    fName="${fileName%.*}"
+    gcc $fullFileName -o $fName -lm
+    ./$fName
+}
+
+#use compile as the name of the command
+#source ./filename.sh in .bashrc
