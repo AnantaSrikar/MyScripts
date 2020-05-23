241b5ee938822816aede44a95219cd4f30982e49
Ananta Srikar <srikarananta01@gmail.com>
2020-05-23 13:58:52 +0530

    Coverted indentation to tabs

--- compileC.sh
+++ compileC.sh
@@ -1,17 +1,17 @@
 function compile(){
-    fullFileName=$1
-    fileName=$(basename "$fullFileName")
-    fName="${fileName%.*}"
-    gcc $fullFileName -o $fName
-    ./$fName
+	fullFileName=$1
+	fileName=$(basename "$fullFileName")
+	fName="${fileName%.*}"
+	gcc $fullFileName -o $fName
+	./$fName
 }
 
 function compileM(){
-    fullFileName=$1
-    fileName=$(basename "$fullFileName")
-    fName="${fileName%.*}"
-    gcc $fullFileName -o $fName -lm
-    ./$fName
+	fullFileName=$1
+	fileName=$(basename "$fullFileName")
+	fName="${fileName%.*}"
+	gcc $fullFileName -o $fName -lm
+	./$fName
 }
 
 #use compile as the name of the command
